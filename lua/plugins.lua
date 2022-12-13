6506fb4e53b9456cfc74a3800e53a58f13619ab3
cyc4188 <106576879@qq.com>
2022-12-13 15:43:13 +0800

    vim-surround

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -63,6 +63,15 @@ return require('packer').startup(function(use)
     use 'terrortylor/nvim-comment'
     -- wilder indent
     use 'gelguy/wilder.nvim'
+    -- vim-surround
+    use({
+    "kylechui/nvim-surround",
+    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+})
 end)
 
---
